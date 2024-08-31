ebb40d8dfb1ef48b515f0f903d7bfb3f34dbd272
gyata <felixasamoah539@gmail.com>
2024-08-31 14:06:51 +0000

    initial commit

---
+++ android/app/src/main/kotlin/africa/ladder/pet/MainActivity.kt
@@ -0,0 +1,6 @@
+package africa.ladder.pet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:personal_expense_tracker/app/app.dart';
+import 'package:personal_expense_tracker/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
